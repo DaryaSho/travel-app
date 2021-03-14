b3de5efdd9fd85f7934350ad7b9bd6f5f4f3814d
Ulyana Zhivotenkova <ulyanazh2@gmail.com>
2021-03-14 08:34:19 +0400

    refactor: change the file structure

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,11 +4,7 @@ import { useHttp } from '../hooks/http.hook';
 import Header from './Header';
 import Footer from './Footer';
 import Home from '../pages/Home';
-import Video from './Video';
-import Slider from './Slider';
-import CountryHeader from './CountryHeader';
-import Description from './CountryDescription';
-
+import Country from '../pages/Country';
 import iceland from '../assets/temp/iceland_main.jpg';
 
 const App: React.FC = () => {
@@ -34,10 +30,7 @@ const App: React.FC = () => {
     <div className='wrapper'>
       <Header />
       <Route path='/' component={Home} exact />
-      <CountryHeader name='Iceland' capital='REYKJAVIK' image={iceland}/>
-      <Description />
-      <Video url='https://vimeo.com/335870770' />
-      <Slider />
+      <Country />
       <Footer />
     </div>
   );
