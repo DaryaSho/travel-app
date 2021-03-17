3e7f7a4618de2d7b4d13e1e85e05383d4643f5f3
Ulyana Zhivotenkova <ulyanazh2@gmail.com>
2021-03-17 06:28:58 +0400

    merge dev

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -14,13 +14,16 @@ const Search: React.FC<{setSearch: any}> = ({ setSearch }) => {
     setInputValue(event.target.value);
   };
 
-  const fetchCountries = async () => {
+  const fetchCountries = useCallback(async () => {
     try {
-      console.log(inputValue);
       const fetched = await request(`/api/countries/countries-list/${inputValue}?lang=${i18n.language}`, 'GET', null);
       setSearch(fetched);
     } catch (e) {}
-  };
+  }, [inputValue]);
+
+  useEffect(() => {
+    fetchCountries();
+  }, [fetchCountries]);
 
   return (
     <div className="search">
@@ -28,6 +31,7 @@ const Search: React.FC<{setSearch: any}> = ({ setSearch }) => {
         value={inputValue}
         onChange={onChangeHandler}
         type="text"
+        autoFocus
         className="search__input"
         placeholder={t('Search country')}/>
       <div className="search__btns">
